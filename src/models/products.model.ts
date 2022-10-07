9fda82d760735d52e0706b379fad5c6e6c0ba428
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-07 01:04:10 -0300

    feat(models): add product models to create and read

---
+++ src/models/products.model.ts
@@ -0,0 +1,27 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../interfaces/product.interface';
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+  
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+      
+    return { id: insertId, name, amount };
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const [products] = await this.connection
+      .execute('SELECT * FROM Trybesmith.Products');
+  
+    return products as Product[];
+  }
+}
