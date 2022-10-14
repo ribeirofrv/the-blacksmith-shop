9378745a02e8fa0468a22a5512d51ba0727e42e3
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-14 14:35:39 -0300

    feat(router): add POST /users route to create users

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import route from './router';
 import 'express-async-errors';
+import ErrorMiddleware from './middlewares/errors';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', route.products);
+app.use('/users', route.users);
+
+app.use(ErrorMiddleware);
 
 export default app;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import products from './products.router';
+import users from './users.router';
 
 export default {
   products,
+  users,
 };
