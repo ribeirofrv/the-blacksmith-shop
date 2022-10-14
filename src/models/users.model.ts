ead8adfa8e759810373d8f36f464a6301079176f
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-14 14:41:33 -0300

    feat(models): add users models to create users

---
+++ src/models/users.model.ts
@@ -0,0 +1,20 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import User from '../interfaces/user.interface';
+
+export default class UserModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(user: User): Promise<number> {
+    const { username, classe, level, password } = user;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?, ?, ?, ?)',
+      [username, classe, level, password],
+    );
+
+    return insertId;
+  }
+}
