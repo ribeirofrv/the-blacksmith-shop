326e77bc7048777a5dff102e4e89e8fc538ae3a8
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-19 14:55:38 -0300

    feat(middleware/auth): groups jwt related functions

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,26 @@
 import jwt, { SignOptions, Secret, JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import { IPayload } from '../interfaces/token.interface';
+import CustomError from '../error/CustomError';
 
 /** Ref.: https://dev.to/juliecherner/authentication-with-jwt-tokens-in-typescript-with-express-3gb1 */
 export const SECRET_KEY: Secret = 'Dragon-Head';
 export const CONFIG: SignOptions = {
   algorithm: 'HS256',
 };
+
+export const generateToken = (user: IPayload): string => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    SECRET_KEY,
+    CONFIG,
+  );
+  return token;
+};
+
+export const decodeToken = (token: string) => jwt.decode(token);
+
+export const verifyToken = (token: string) => jwt.verify(token, SECRET_KEY);
 
 export interface CustomRequest extends Request {
   token: string | JwtPayload;
@@ -15,13 +30,13 @@ export const auth = async (req: Request, _res: Response, next: NextFunction) => 
   try {
     const token = req.header('Authorization');
 
-    if (!token) throw new Error('401|Token not found');
+    if (!token) throw new CustomError(401, 'Token not found');
 
-    const decoded = jwt.verify(token, SECRET_KEY);
+    const decoded = verifyToken(token);
     (req as CustomRequest).token = decoded;
 
     next();
   } catch (error) {
-    throw new Error('401|Expired or invalid token');
+    throw new CustomError(401, 'Expired or invalid token');
   }
 };
