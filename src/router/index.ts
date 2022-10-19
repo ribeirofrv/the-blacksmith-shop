27a11cbd860fb79948e99b157d8535abd048eb95
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-19 15:01:58 -0300

    feat(router): add /login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import products from './products.router';
 import users from './users.router';
 import orders from './orders.router';
+import login from './login.router';
 
 export default {
+  login,
   products,
   users,
   orders,
