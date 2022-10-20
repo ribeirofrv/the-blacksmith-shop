5e07bd213e8e8d09dfa38639c8748d4b473db37c
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-19 23:18:01 -0300

    fix(middleware): Adjust error middleware to handle custom errors
    Disclaimer: Help from Specialist Will Marcondes and classmate José Lucas R. Santos

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -2,19 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import CustomError from '../error/CustomError';
 
 const ErrorMiddleware = (
-  err: Error,
+  err: CustomError,
   _req: Request,
   res: Response,
   _next: NextFunction,
 ): Response => {
-  if (err as CustomError) {
-    console.log(err);
-    
-    // return res.status(err.statusCode).json({ message: err.message });
+  if (err.statusCode) {
+    return res.status(err.statusCode).json({ message: err.message });
   }
   console.log(':: ERROR:', err);
 
-  return res.status(500).json({ message: err.message });
+  return res.status(500).json({ message: err });
 };
 
 export default ErrorMiddleware;
