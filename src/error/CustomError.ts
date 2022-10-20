09054a3809c2c5c9e8b4239609903fee6ae5a888
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-20 01:17:44 -0300

    fix(error/CustomError): remove unnecessary code

--- src/error/CustomError.ts
+++ src/error/CustomError.ts
@@ -1,26 +1,8 @@
-/** Blog */
-// export default class CustomError extends Error {
-//   statusCode: number;
-
-//   constructor(statusCode: number, message: string) {
-//     super(message);
-
-//     Object.setPrototypeOf(this, new.target.prototype);
-//     this.name = Error.name;
-//     this.statusCode = statusCode;
-//     Error.captureStackTrace(this);
-//   }
-// }
-
-/** live lecture */
 export default class CustomError extends Error {
-  statusCode: number;
-
   message: string;
 
-  constructor(statusCode: number, message: string) {
+  constructor(message: string) {
     super(message);
-    this.statusCode = statusCode;
     this.message = message;
   }
 }
