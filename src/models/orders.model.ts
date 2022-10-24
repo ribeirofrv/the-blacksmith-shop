a42ef3e3f809dc09781df834f9cd5dd42f5d91a8
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-24 17:16:46 -0300

    feat(models): add order models to find a specific order and create new orders

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,13 +1,13 @@
-import { Pool } from 'mysql2/promise';
-import IOrder from '../interfaces/order.interface';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import IOrder, { IOrderById } from '../interfaces/order.interface';
 
 export default class OrderModel {
   public connection: Pool;
-  
+
   constructor(connection: Pool) {
     this.connection = connection;
   }
-  
+
   public async getAll(): Promise<IOrder[]> {
     const query = [
       'SELECT Orders.id as id,',
@@ -18,9 +18,35 @@ export default class OrderModel {
       'WHERE Orders.id = Products.orderId',
       'GROUP BY Orders.id;',
     ].join(' ');
-  
+
     const [orders] = await this.connection.execute(query);
-  
+
     return orders as IOrder[];
   }
+
+  public async create(order: number): Promise<number> {
+    const query = 'INSERT INTO Trybesmith.Orders (userId) VALUES (?)';
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      query,
+      [order]
+    );
+    return insertId;
+  }
+
+  public async getOrderByOrderId(id: number): Promise<IOrderById[]> {
+    const query = [
+      'SELECT Orders.userId as userId,',
+      'JSON_ARRAYAGG(Products.id) as productsIds',
+      'FROM Trybesmith.Orders',
+      'JOIN Trybesmith.Products',
+      'WHERE Orders.id = Products.orderId',
+      'AND Orders.id = ?',
+      'GROUP BY Orders.id;',
+    ].join(' ');
+
+    const [order] = await this.connection.execute(query, [id]);
+
+    return order as IOrderById[];
+  }
 }
+// SELECT * FROM `Products` LIMIT 100;
