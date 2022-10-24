ec3c7307cc6923c72b3472f063c249d2e1d392db
Fran Ribeiro <ribeirofrv@gmail.com>
2022-10-24 17:20:02 -0300

    feat(router): add POST /orders route

--- src/router/orders.router.ts
+++ src/router/orders.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { auth } from '../middlewares/auth';
 import OrdersController from '../controllers/orders.controller';
 
 const router = Router();
 const ordersController = new OrdersController();
 
 router.get('/', ordersController.getAll);
+router.post('/', auth, ordersController.register);
 
 export default router;
